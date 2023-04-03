393d753fea57c11a581ad91aaf2656aab83ad862
sawada1 <khaledsawada1@gmail.com>
2023-04-03 14:04:17 +0200

    changing

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/rotana/',
-RewriteEngine: On,
-Rewritebase: '/',
-RewriteRule:'^index\.html$ - [L]',
-RewriteCond:'%{REQUEST_FILENAME} !-f',
-RewriteCond: '%{REQUEST_FILENAME} !-d',
-RewriteRule: '. /index.html [L]'
+
 })
